0a78d4f8ff2d371f13eabb12d2f4c355e5085b1a
Anh Tranngoc <manhhomienbienthuy@yahoo.com.vn>
2018-11-20 16:04:07 +0700

    Update scripts

--- setup.sh
+++ setup.sh
@@ -8,12 +8,10 @@ for filename in .[a-z]*; do
   fi
 done
 
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > git-prompt.sh
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > git-completion.sh
-ln -s ${PWD}/git-prompt.sh ~/
-ln -s ${PWD}/git-completion.sh ~/
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/git-prompt.sh
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/git-completion.sh
+
 
 vim +PluginInstall "+mkspell vi vi_VN" +qall
-
 mkdir ~/.vim/spell
-ln -s ${PWD}/vi.utf-8.spl ~/.vim/spell/
+mv ./vi.utf-8.spl ~/.vim/spell/
